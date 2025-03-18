3678d841ecadf53bf56099dbed05f271cdf19d35
phrubel <phrubel42@gmail.com>
2025-03-19 02:16:00 +0600

    stream filter added

--- src/models/streamModel.js
+++ src/models/streamModel.js
@@ -7,6 +7,15 @@ const StreamSchema = mongoose.Schema({
   isLive: { type: Boolean, default: false },
   upcomming: { type: Boolean, default: true },
   startDate: { type: Date },
+  standard: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'standard',
+  }, // e.g., "10th Grade"
+  boardId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'board',
+    // required: "Board Required",
+  },
 });
 
 const Stream = mongoose.model('Stream', StreamSchema);
